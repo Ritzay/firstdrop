a9a08a472514156b75e8328025f2d4f069a7b878
macbookpro <macbookpro@example.com>
2024-08-29 10:59:31 -0700

    uploading of the completed project

---
+++ wp-content/themes/leocrypto/assets/js/headroomf269f269f269.js
@@ -0,0 +1,11 @@
+;
+(function($) {
+    "use strict";
+    $(document).ready(function() {
+        var myElement = document.querySelector("#headroom");
+        var headroom = new Headroom(myElement, {
+            offset: 280
+        });
+        headroom.init();
+    });
+})(jQuery);
